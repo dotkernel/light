5a391f3ae1c10af3c5c1d5f9bbad9fa589679e97
alexmerlin <alex.merlin.1985@gmail.com>
2024-09-30 10:43:21 +0300

    Tweaked page routes
    Signed-off-by: alexmerlin <alex.merlin.1985@gmail.com>

--- src/Page/src/RoutesDelegator.php
+++ src/Page/src/RoutesDelegator.php
@@ -4,26 +4,22 @@ declare(strict_types=1);
 
 namespace Light\Page;
 
-use Fig\Http\Message\RequestMethodInterface;
 use Light\Page\Controller\PageController;
 use Mezzio\Application;
 use Psr\Container\ContainerInterface;
+
+use function assert;
 
 class RoutesDelegator
 {
     public function __invoke(ContainerInterface $container, string $serviceName, callable $callback): Application
     {
-        /** @var Application $app */
         $app = $callback();
+        assert($app instanceof Application);
 
         $app->get('/', [PageController::class], 'home');
 
-        $app->route(
-            '/page[/{action}]',
-            [PageController::class],
-            [RequestMethodInterface::METHOD_GET, RequestMethodInterface::METHOD_POST],
-            'page'
-        );
+        $app->get('/page[/{action}]', [PageController::class], 'page');
 
         return $app;
     }
